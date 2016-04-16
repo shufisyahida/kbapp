f9761755ca814e5372bff28bfcab770bf149065e
Shufi <semangkaforever@gmail.com>
2016-04-17 03:04:20 +0700

    modifying app

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,23 @@
-import './main.css';
+import './assets/css/main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import {Router, Route, browserHistory} from 'react-router';
 
 import alt from './libs/alt';
 import storage from './libs/storage';
 import persist from './libs/persist';
 
+import Home from './views/home/home.jsx';
+import Profile from './views/profile/profile.jsx';
+
 persist(alt, storage, 'app');
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const appRouter = (
+	<Router history={browserHistory}>
+		<Router path="/" component={Home}/>
+		<Router path="/profile" component={Profile}/>
+	</Router>
+);
+
+ReactDOM.render(appRouter, document.getElementById('app'));
